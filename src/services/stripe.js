940485e593e492040d562c45edea09e579430fc3
Edgar Lopez Lopez <edgarlopezlopez@Edgars-MacBook-Air.local>
2022-07-06 13:52:12 -0500

    stripe url fix

--- src/services/stripe.js
+++ src/services/stripe.js
@@ -2,13 +2,15 @@ import axios from 'axios'
 
 const LOOKUP_KEY = 'OTS-PREMIUM'
 
+const apiUrl = 'https://ots-back.herokuapp.com'
+
 export const createStripeCheckoutSession = async (userId) => {
   try {
     const token = window.localStorage.getItem('token')
     if (!token) {
       return null
     }
-    const { data } = await axios.post(process.env.STRAPI_API_URL + '/api/create-checkout-session', {
+    const { data } = await axios.post(apiUrl + '/api/create-checkout-session', {
       lookup_key: LOOKUP_KEY,
       user_id: userId
     },
@@ -29,7 +31,7 @@ export const createStripePortalSession = async (sessionId, subId) => {
     if (!token) {
       return null
     }
-    const { data } = await axios.post(process.env.STRAPI_API_URL + '/api/create-portal-session', {
+    const { data } = await axios.post(apiUrl + '/api/create-portal-session', {
       session_id: sessionId,
       sub_id: subId
     },
